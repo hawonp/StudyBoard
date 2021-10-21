4b79e4b540c3044e7ecef96f6f941306d9377ceb
PKHong <pyungkang1004@gmail.com>
2021-10-21 15:13:58 +0900

    delete static/js,image,font files

--- studyboard/studyboard/settings.py
+++ studyboard/studyboard/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-(##ibs3_8wm(zoazhe$9bchql+2v&5l&&&yfp)8t1n!2ycfuan
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
