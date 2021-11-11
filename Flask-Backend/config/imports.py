1e72275a441e641304b562b7422852a60014ca9c
Jeongho Shin <45681556+je0shin@users.noreply.github.com>
2021-11-11 12:49:58 +0900

    merging

--- Flask-Backend/config/imports.py
+++ Flask-Backend/config/imports.py
@@ -2,7 +2,7 @@ import json
 import mariadb
 import sys 
 import os
-from flask import Flask, request, Blueprint, url_for, redirect
+from flask import Flask, request, Blueprint, url_for, redirect, abort
 from flask_dance.contrib.google import make_google_blueprint, google
 from flask_restful import Resource, Api
 from marshmallow import Schema, fields
