2d5689b2261619edca60dff69e5d04bf9a174824
Hawon Park <hawonp@gmail.com>
2021-11-12 14:26:48 +0900

    Update postedit.js

--- Web-front/pages/postedit.js
+++ Web-front/pages/postedit.js
@@ -2,7 +2,7 @@ import Container from "@mui/material/Container";
 import * as React from "react";
 import EditPost from "../components/EditPost";
 import {useRouter} from "next/router";
-
+import PostCard from "../components/PostCard"
 
 
 export default function Postedit({ }) {
@@ -10,7 +10,7 @@ export default function Postedit({ }) {
     // const { id, user, title, images ,content, tags } = router.query;
     return (
         <Container>
-            <EditPost postCard={postCard}/>
+            <EditPost PostCard={PostCard}/>
         </Container>
     )
 }
