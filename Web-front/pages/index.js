ba95a9395d2ccec0676210fb9b8875d1de9882ec
PyungkangHong <53274042+PyungkangHong094@users.noreply.github.com>
2021-11-22 00:40:58 +0900

    -

--- Web-front/pages/index.js
+++ Web-front/pages/index.js
@@ -6,17 +6,17 @@ import { fontSize, padding } from '@mui/system';
 export default function Home() {
     return (
         <Container>
-            <div class="background" data-aos="fade-up">
-                <div class="row gx-0">
-                    <div class="col-lg-6 d-flex flex-column justify-content-center" data-aos="fade-up" data-aos-delay="200">
-                        <div class="content">
+            <div data-aos="fade-up">
+                <div>
+                    <div data-aos="fade-up" data-aos-delay="200">
+                        <div>
                             <h3>Who Need Our service?</h3>
                             <h2>내용이 필요합니다 </h2>
                             <p>내용이 필요합니다</p>
-                            <div class="text-center text-lg-start">
+                            <div>
                                 <a href="#" class="btn-read-more d-inline-flex align-items-center justify-content-center align-self-center">
                                     <span>Need to find answer?</span>
-                                    <i class="bi bi-arrow-right"></i>
+                                    <i></i>
                                 </a>
                             </div>
                         </div>
