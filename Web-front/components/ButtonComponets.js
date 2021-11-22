6326fe5f5a8729714e00bb104c45f96a7689bc83
PyungkangHong <53274042+PyungkangHong094@users.noreply.github.com>
2021-11-23 00:38:11 +0900

    update icon card show

--- Web-front/components/ButtonComponets.js
+++ Web-front/components/ButtonComponets.js
@@ -1,37 +1,42 @@
-import IconButton from '@mui/material/IconButton';
-import FavoriteIcon from '@mui/icons-material/Favorite';
-import ThumbUpIcon from '@mui/icons-material/ThumbUp';
-import SmsIcon from '@mui/icons-material/Sms';
-import BookmarkIcon from '@mui/icons-material/Bookmark';
-import ShareIcon from '@mui/icons-material/Share';
-import CardActions from '@mui/material/CardActions';
-import React from 'react';
-import FlagIcon from '@mui/icons-material/Flag';
+import IconButton from "@mui/material/IconButton";
+import FavoriteIcon from "@mui/icons-material/Favorite";
+import ThumbUpIcon from "@mui/icons-material/ThumbUp";
+import SmsIcon from "@mui/icons-material/Sms";
+import BookmarkIcon from "@mui/icons-material/Bookmark";
+import ShareIcon from "@mui/icons-material/Share";
+import CardActions from "@mui/material/CardActions";
+import React from "react";
+import FlagIcon from "@mui/icons-material/Flag";
 
 const CountNumber = ({ style, children }) => {
-    return <div style={{ fontSize: '0.8rem', fontWeight: 'bold', ...style }}> {children} </div>;
+  return (
+    <div style={{ fontSize: "0.8rem", fontWeight: "bold", ...style }}>
+      {" "}
+      {children}{" "}
+    </div>
+  );
 };
 
 export default function ButtonComponets() {
-    return (
-        <CardActions disableSpacing sx={{ justifyContent: 'end' }}>
-            <IconButton aria-label="favorites">
-                <FavoriteIcon sx={{ fontSize: '1.2rem' }} />
-                <CountNumber>4 Likes</CountNumber>
-            </IconButton>
-            {/* post_like_count */}
+  return (
+    <CardActions disableSpacing sx={{ justifyContent: "end" }}>
+      <IconButton aria-label="favorites" sx={{ borderRadius: "4px" }}>
+        <FavoriteIcon sx={{ fontSize: "1.2rem" }} />
+        <CountNumber>4 Likes</CountNumber>
+      </IconButton>
+      {/* post_like_count */}
 
-            {/* <IconButton aria-label="thoumup">
+      {/* <IconButton aria-label="thoumup">
                 <ThumbUpIcon sx={{ fontSize: '1.2rem' }}/>
             </IconButton>
             <CountNumber>: 0</CountNumber> */}
-            {/* Comment button &  */}
+      {/* Comment button &  */}
 
-            <IconButton aria-label="SmsIcon">
-                <SmsIcon sx={{ fontSize: '1.2rem' }} />
-                <CountNumber>5 Replies</CountNumber>
-            </IconButton>
-            {/* post_reply_count */}
-        </CardActions>
-    );
+      <IconButton aria-label="SmsIcon" sx={{ borderRadius: "4px" }}>
+        <SmsIcon sx={{ fontSize: "1.2rem" }} />
+        <CountNumber>5 Replies</CountNumber>
+      </IconButton>
+      {/* post_reply_count */}
+    </CardActions>
+  );
 }
