f083e19ced3840625560d6b96a04d875ee0db703
Jeongho Shin <45681556+je0shin@users.noreply.github.com>
2021-12-01 03:15:39 +0900

    [WIP] notif
    FIX for reply to replies
    - WIP notif getting data

--- Flask-Backend/query/notification_query.py
+++ Flask-Backend/query/notification_query.py
@@ -31,6 +31,9 @@ def get_user_notifications(user_id):
         for result in rv:
             res.append(dict(zip(row_headers,result)))
 
+        #Set the notifications as seen
+        query = "UPDATE Notification SET notification_seen=1 WHERE user_id=?"
+        cursor.execute(query, values)
         #Closing cursor
         cursor.close()
         conn.commit()
@@ -48,3 +51,56 @@ def get_user_notifications(user_id):
 ##########################################################
 #                         DELETE                         #
 ##########################################################
+#Delete notification
+def delete_notification(notification_id):
+    res = 1
+    try:
+        #Obtain DB cursor
+        conn = get_connection()
+        cursor = conn.cursor()
+
+        #Set up query statement and values
+        query = "DELETE FROM Notification WHERE notification_id=?"
+        values = (notification_id,)
+
+        #Getting data from table
+        print("Deleting with query", query, " and values ", values)
+        cursor.execute(query, values)
+        
+        #Closing cursor
+        cursor.close()
+        conn.commit()
+        conn.close()
+        
+    except mariadb.Error as e:
+        print(f"Error adding entry to database: {e}")
+        res = 0
+    
+    return res
+
+#Delete all notifications
+def delete_all_notifications(user_id):
+    res = 1
+    try:
+        #Obtain DB cursor
+        conn = get_connection()
+        cursor = conn.cursor()
+
+        #Set up query statement and values
+        query = "DELETE FROM Notification WHERE user_id=?"
+        values = (user_id,)
+
+        #Getting data from table
+        print("Deleting with query", query, " and values ", values)
+        cursor.execute(query, values)
+        
+        #Closing cursor
+        cursor.close()
+        conn.commit()
+        conn.close()
+        
+    except mariadb.Error as e:
+        print(f"Error adding entry to database: {e}")
+        res = 0
+    
+    return res
