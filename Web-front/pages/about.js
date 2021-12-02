b629961d7d906af9cba42d14c0e2a26b7420ae09
PyungkangHong <53274042+PyungkangHong094@users.noreply.github.com>
2021-12-02 23:48:47 +0900

    update none functional page

--- Web-front/pages/about.js
+++ Web-front/pages/about.js
@@ -1,10 +1,72 @@
 import Head from "next/head";
+import Link from "next/link";
 import AboutPageNav from "../components/AboutPageNav";
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+
+import Chip from "@mui/material/Chip";
+import Stack from "@mui/material/Stack";
 
 export default function About() {
   return (
-    <div>
-      <AboutPageNav />
-    </div>
+    <Box
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+      }}
+    >
+      <div
+        style={{
+          marginTop: ".8rem",
+          marginBottom: "2rem",
+          padding: "1rem 1rem",
+        }}
+      >
+        <Button
+          variant="contained"
+          sx={{ marginRight: "0.5rem", borderRadius: "20px" }}
+        >
+          <Link href="/about">
+            <a style={{ textDecoration: "none", color: "white" }}>
+              <div>About</div>
+            </a>
+          </Link>
+        </Button>
+        <Button
+          variant="contained"
+          sx={{ marginRight: "0.5rem", borderRadius: "20px" }}
+        >
+          <Link href="/leadership">
+            <a style={{ textDecoration: "none", color: "white" }}>
+              <div>Leadership</div>
+            </a>
+          </Link>
+        </Button>
+        <Button variant="contained" sx={{ borderRadius: "20px" }}>
+          <Link href="/contact">
+            <a style={{ textDecoration: "none", color: "white" }}>
+              <div>Contact</div>
+            </a>
+          </Link>
+        </Button>
+      </div>
+
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <Box>icon</Box>
+        <Box>Studyboard Leadrship team</Box>
+        <Box sx={{ display: "flex" }}>
+          <Box>하원</Box>
+          <Box>평강</Box>
+          <Box>정호</Box>
+        </Box>
+      </Box>
+    </Box>
   );
 }
--- Web-front/pages/contact.js
+++ Web-front/pages/contact.js
@@ -1,12 +1,156 @@
 import Head from "next/head";
+import AboutPageNav from "../components/AboutPageNav";
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import DraftsOutlinedIcon from "@mui/icons-material/DraftsOutlined";
+import FmdGoodIcon from "@mui/icons-material/FmdGood";
+import CallIcon from "@mui/icons-material/Call";
+import FaxIcon from "@mui/icons-material/Fax";
+import MailIcon from "@mui/icons-material/Mail";
+import Link from "next/link";
+import Card from "@mui/material/Card";
+import image from "next/image";
+import Paper from "@mui/material/Paper";
 
 export default function Contact() {
   return (
-    <div>
-      <Head>
-        <title>StudyBoard</title>
-      </Head>
-      Contact
-    </div>
+    <Box
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+      }}
+    >
+      <div
+        style={{
+          marginTop: ".8rem",
+          marginBottom: "2rem",
+          padding: "1rem 1rem",
+          MaxWidth: "1450px",
+        }}
+      >
+        <Button
+          variant="contained"
+          sx={{ marginRight: "0.5rem", borderRadius: "20px" }}
+        >
+          <Link href="/about">
+            <a style={{ textDecoration: "none", color: "white" }}>
+              <div>About</div>
+            </a>
+          </Link>
+        </Button>
+        <Button
+          variant="contained"
+          sx={{ marginRight: "0.5rem", borderRadius: "20px" }}
+        >
+          <Link href="/leadership">
+            <a style={{ textDecoration: "none", color: "white" }}>
+              <div>Leadership</div>
+            </a>
+          </Link>
+        </Button>
+        <Button variant="contained" sx={{ borderRadius: "20px" }}>
+          <Link href="/contact">
+            <a style={{ textDecoration: "none", color: "white" }}>
+              <div>Contact</div>
+            </a>
+          </Link>
+        </Button>
+      </div>
+
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <Box>
+          <DraftsOutlinedIcon sx={{ fontSize: "5rem" }} />
+        </Box>
+        <div style={{ fontSize: "45px", marginBottom: "3.2rem" }}>
+          {"Contact Studyboard & Feedback"}
+        </div>
+
+        <Box sx={{ display: "flex" }}>
+          <Paper
+            elevation={3}
+            sx={{
+              display: "flex",
+              flexDirection: "column",
+              justifyContent: "center",
+              alignItems: "center",
+              height: "140px",
+              width: "170px",
+              margin: "24px",
+              padding: "30px",
+            }}
+          >
+            <div>
+              <FmdGoodIcon sx={{ fontSize: "3rem" }} />
+            </div>
+            <div>something</div>
+            <div>information</div>
+          </Paper>
+          <Paper
+            elevation={3}
+            sx={{
+              display: "flex",
+              flexDirection: "column",
+              justifyContent: "center",
+              alignItems: "center",
+              height: "140px",
+              width: "170px",
+              margin: "24px",
+              padding: "30px",
+            }}
+          >
+            <div>
+              <MailIcon sx={{ fontSize: "3rem" }} />
+            </div>
+            <div>something</div>
+            <div>information</div>
+          </Paper>
+          <Paper
+            elevation={3}
+            sx={{
+              display: "flex",
+              flexDirection: "column",
+              justifyContent: "center",
+              alignItems: "center",
+              height: "140px",
+              width: "170px",
+              margin: "24px",
+              padding: "30px",
+            }}
+          >
+            <div>
+              <CallIcon sx={{ fontSize: "3rem" }} />
+            </div>
+            <div>something</div>
+            <div>information</div>
+          </Paper>
+          <Paper
+            elevation={3}
+            sx={{
+              display: "flex",
+              flexDirection: "column",
+              justifyContent: "center",
+              alignItems: "center",
+              height: "140px",
+              width: "170px",
+              margin: "24px",
+              padding: "30px",
+            }}
+          >
+            <div>
+              <FaxIcon sx={{ fontSize: "3rem" }} />
+            </div>
+            <div>something</div>
+            <div>information</div>
+          </Paper>
+        </Box>
+      </Box>
+    </Box>
   );
 }
